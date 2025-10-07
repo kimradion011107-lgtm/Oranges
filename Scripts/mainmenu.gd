7ba8266a7b6d05cc6780d31f5c989dc899ce553e
Radion Kim <kim.radion011107@gmail.com>
2025-10-07 16:45:35 +0500

    Initial commit

---
+++ Scripts/mainmenu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://options.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()

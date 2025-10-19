e9912343ddf9eae22c8f52f52dd4b6de4b8efb97
Radion Kim <kim.radion011107@gmail.com>
2025-10-19 21:35:26 +0500

    UItheme1

--- data/scripts/ui/mainmenu.gd
+++ data/scripts/ui/mainmenu.gd
@@ -3,11 +3,11 @@ extends Node2D
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://assets/Scenes/game.tscn")
+	get_tree().change_scene_to_file("res://data/Scenes/game.tscn")
 
 
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://assets/Scenes/options.tscn")
+	get_tree().change_scene_to_file("res://data/Scenes/options.tscn")
 
 
 func _on_exit_pressed() -> void:
